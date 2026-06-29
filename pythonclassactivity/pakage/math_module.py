3cd29b10b9f1906ca1699b59635c384c583434e4
Shalu Jain <ruhirj14@gmail.com>
2026-06-29 17:31:13 +0530

    math module

---
+++ pythonclassactivity/pakage/math_module.py
@@ -0,0 +1,22 @@
+def addition(a,b):
+    return a+b
+
+
+def sub(a,b):
+    return a-b
+
+
+def multiply(a,b):
+    return a*b
+
+
+def divide(a,b):
+    return a%b
+
+
+def square(a):
+    return a**2
+
+
+def average(a,b,c,d,e):
+    return sum([a,b,c,d,e])/5
